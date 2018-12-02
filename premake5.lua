330ab64a8c28bd0a040070c8d4315b1d0e078f3f
bqqbarbhg <samuli.1995@hotmail.com>
2018-12-02 21:07:52 +0200

    Return empty string on non-Windows

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 function os.winSdkVersion()
-    local reg_arch = iif( os.is64bit(), "\\Wow6432Node\\", "\\" )
-    local sdk_version = os.getWindowsRegistry( "HKLM:SOFTWARE" .. reg_arch .."Microsoft\\Microsoft SDKs\\Windows\\v10.0\\ProductVersion" )
-    if sdk_version ~= nil then return sdk_version end
+	local reg_arch = iif( os.is64bit(), "\\Wow6432Node\\", "\\" )
+	local sdk_version = os.getWindowsRegistry( "HKLM:SOFTWARE" .. reg_arch .."Microsoft\\Microsoft SDKs\\Windows\\v10.0\\ProductVersion" )
+	if sdk_version ~= nil then return sdk_version end
+	return ""
 end
 
 function symbols_on()
