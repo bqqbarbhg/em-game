0773a44aced80c7d4d59c2337fbf9ac88d56b130
bqqbarbhg <samuli.1995@hotmail.com>
2018-12-03 23:27:51 +0200

    Attempt to install emsdk

--- install-travis.sh
+++ install-travis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-if [ ! -f premake_build/premake5 ]; then
-	mkdir premake_build
-	cd premake_build
+if [ ! -f travis_build/premake5 ]; then
+	mkdir travis_build
+	cd travis_build
 	wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha12/premake-5.0.0-alpha12-src.zip -O premake.zip
 	unzip -qq premake.zip
 	cd premake-5.0.0-alpha12/build/gmake.unix
@@ -10,3 +10,18 @@ if [ ! -f premake_build/premake5 ]; then
 	mv premake-5.0.0-alpha12/bin/release/premake5 premake5
 	cd ..
 fi
+
+if [ ! -d travis_build/emsdk ]; then
+	mkdir travis_build
+	git clone https://github.com/juj/emsdk.git
+	cd emsdk
+
+	./emsdk install latest
+	./emsdk activate latest
+
+	cd ../..
+fi
+
+source travis_build/emsdk/emsdk_env.sh
+
+
